53985e9845fa9adc9038b0e2024009b2d66ddb7e
radhakrishnakota <radhakrishna6246@gmail.com>
2024-05-13 21:40:21 +0100

    New package

---
+++ src/main/java/oops/Square.java
@@ -0,0 +1,11 @@
+package oops;
+
+public class Square {
+    int side;
+    public void getarea()
+    {
+      int area=  this.side * this.side;
+              System.out.println("Area is "+ area);
+
+    }
+}
---
+++ src/main/java/oops/Student.java
@@ -0,0 +1,22 @@
+package oops;
+
+public class Student {
+    String name;
+    int age;
+    int marks;
+
+    public void greet() {
+        System.out.println("Hi, my name is " + this.name + " I got " + this.marks + " marks");
+    }
+
+    public static void main(String[] args) {
+        Student radha = new Student();
+        radha.name = "radha";
+        radha.age = 12;
+        radha.marks = 55;
+        radha.greet();
+        Square S1= new Square();
+        S1.side=5;
+        S1.getarea();
+    }
+}
